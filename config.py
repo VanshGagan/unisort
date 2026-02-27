0d664f5c47eb88baaae51ea0e5549ff591faa61e
Vansh <vansh.gagan@uzh.ch>
2026-02-27 15:46:03 +0100

    first setup version: user is able to change the source-folder

--- config.py
+++ config.py
@@ -4,3 +4,7 @@ def load_config():
     with open('config.json', 'r') as f:
         config = json.load(f)
     return config
+
+def save_config(config_file):
+    with open("config.json", "w") as f:
+        json.dump(config_file, f, indent=4)
--- setup.py
+++ setup.py
@@ -1,2 +1,28 @@
+import json
+import config
+
+
+def update_source():
+    cfg = config.load_config()
+    user_source = input("Please provide the path for your source-folder:   ")
+    cfg["source_folder"] = user_source
+    config.save_config(cfg)
+
+
+
+
 def run_setup():
-    print("SETUP!")
+    print("--------// SETUP //--------")
+    while True:
+        ask_continue = input("Warning: Existing config will be overwritten.\n Continue? (y/n)   ")
+        if ask_continue.lower() == "y":
+            print("Proceeding with setup...")
+            break
+        elif ask_continue.lower() == "n":
+            return
+        else:
+            print("Please write y or n")
+
+    update_source()
+
+
